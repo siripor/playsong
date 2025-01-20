7efde3a1f9fbcc851a3367467e1196dc3b48a5f0
siriporn_yai <84725871+siripor@users.noreply.github.com>
2025-01-20 20:44:22 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const song = document.getElementById("song")
+const Btnplay = document.getElementById("play")
+const Btnpause = document.getElementById("pause")
+const songProgressBar = document.getElementById("seek")
+const Btnplayrewind = document.getElementById("playrewind")
+const Btnplayfast = document.getElementById("playfast")
+
+Btnpause.style.display="none"
+
+Btnplay.onclick=()=>{
+  song.play();
+Btnpause.style.display=""
+Btnplay.style.display="none"  
+}
+
+Btnpause.onclick=()=>{
+  song.pause();
+Btnpause.style.display="none"
+Btnplay.style.display=""  
+}
+
+song.onloadedmetadata=()={
+  songProgressBar.max = song.duration;
+  songProgressBar.value = song.currentTime;
+
+}
+
+if(song()){
+  setInterval(()=>{
+    songProgressBar.value = song.currentTime;
+  },500);
+}
+
+songProgressBar.onchange=()={
+  song.currentTime = songProgressBar.value;
+}
+
+Btnplayrewind.onclick=()=>{
+  song.currentTime -= 10;
+}
+Btnplayfast.onclick=()=>{
+  song.currentTime += 10;
+}
